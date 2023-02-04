5735c6e3678b67b3ccc6d119f97c4259e14194ba
yebh17 <sunnyrebels0@gmail.com>
2023-02-05 00:35:05 +0100

    Add combined ecdf graph

--- plot.py
+++ plot.py
@@ -375,10 +375,120 @@ def zerotier():
 
     return raw_graph(), histogram_graph(), ecdf_graph()
 
+def ecdfs_comparision_graph():
+    # Create figure and axis objects
+    fig, ax = plt.subplots()
+
+    # Create a directory to store all the graphs
+    wg_dir = "graphs/wireguard"
+    os.makedirs(wg_dir, exist_ok=True)
+
+    # Open the wireguard log file
+    with open("wireguard_results/metrics.log", "r") as f:
+        content = f.read()
+
+    # Find all decimal values using regular expression
+    decimal_values = re.findall(r"-?\d+\.\d+", content)
+
+    # Convert the values to float
+    decimal_values = [float(x) for x in decimal_values]
+
+    resp_times_wg = [num * 1000 for num in decimal_values]
+
+    resp_times_wg = [round(num, 3) for num in resp_times_wg]
+
+    n1 = len(resp_times_wg)
+    x1 = np.sort(resp_times_wg)
+    y1 = np.arange(1, n1 + 1) / n1
+    ax.plot(x1, y1, color="green", marker='.', linestyle='none', markersize=5, label='WireGuard')
+    
+    # Open the ovpn log file
+    with open("ovpn_results/metrics.log", "r") as f:
+        content = f.read()
+
+    # Find all decimal values using regular expression
+    decimal_values = re.findall(r"-?\d+\.\d+", content)
+
+    # Convert the values to float
+    decimal_values = [float(x) for x in decimal_values]
+
+    resp_times_ovpn = [num * 1000 for num in decimal_values]
+
+    resp_times_ovpn = [round(num, 3) for num in resp_times_ovpn]
+
+    n2 = len(resp_times_ovpn)
+    x2 = np.sort(resp_times_ovpn)
+    y2 = np.arange(1, n2 + 1) / n2
+    ax.plot(x2, y2, marker='.', linestyle='none', markersize=5, color="red", label='OpenVPN')
+
+    # Open the softether log file
+    with open("softether_results/metrics.log", "r") as f:
+        content = f.read()
+
+    # Find all decimal values using regular expression
+    decimal_values = re.findall(r"-?\d+\.\d+", content)
+
+    # Convert the values to float
+    decimal_values = [float(x) for x in decimal_values]
+
+    resp_times_se = [num * 1000 for num in decimal_values]
+
+    resp_times_se = [round(num, 3) for num in resp_times_se]
+
+    n3 = len(resp_times_se)
+    x3 = np.sort(resp_times_se)
+    y3 = np.arange(1, n3 + 1) / n3
+    ax.plot(x3, y3, marker='.', linestyle='none', markersize=5, color="blue", label='Softether')
+    
+    # Open the tinc log file
+    with open("tinc_results/metrics.log", "r") as f:
+        content = f.read()
+
+    # Find all decimal values using regular expression
+    decimal_values = re.findall(r"-?\d+\.\d+", content)
+
+    # Convert the values to float
+    decimal_values = [float(x) for x in decimal_values]
+
+    resp_times_tinc = [num * 1000 for num in decimal_values]
+
+    resp_times_tinc = [round(num, 3) for num in resp_times_tinc]
+
+    n4 = len(resp_times_tinc)
+    x4 = np.sort(resp_times_tinc)
+    y4 = np.arange(1, n4 + 1) / n4
+    ax.plot(x4, y4, marker='.', linestyle='none', markersize=5, color="purple", label='Tinc')
+    
+    # Open the zerotier log file
+    with open("softether_results/metrics.log", "r") as f:
+        content = f.read()
+
+    # Find all decimal values using regular expression
+    decimal_values = re.findall(r"-?\d+\.\d+", content)
+
+    # Convert the values to float
+    decimal_values = [float(x) for x in decimal_values]
+
+    resp_times_zt = [num * 1000 for num in decimal_values]
+
+    resp_times_zt = [round(num, 3) for num in resp_times_zt]
+
+    n5 = len(resp_times_zt)
+    x5 = np.sort(resp_times_zt)
+    y5 = np.arange(1, n5 + 1) / n5
+    ax.plot(x5, y5, marker='.', linestyle='none', markersize=5, color="black", label='Zerotier')
+
+    plt.xlim(0, 30)
+    plt.ylim(0, 0.4)
+    ax.legend()
+    
+    # Save the plot to a file
+    plt.savefig("graphs/combined_ecdf.jpg")
+
 def main():
     argumentList = sys.argv[1:]
-    options = "wostz"
-    long_options = ["wireguard", "openvpn", "softether", "tinc", "zerotier"] 
+    options = "wostze"
+    long_options = ["wireguard", "openvpn", "softether", "tinc", "zerotier", "ecdf"] 
     try:
         arguments, values = getopt.getopt(argumentList, options, long_options)
 
@@ -403,6 +513,9 @@ def main():
             elif currentArgument in ("-z", "--zerotier"):
                 print ("Loading zerotier Graph")
                 zerotier()
+            elif currentArgument in ("-e", "--ecdf"):
+                print ("Loading combined ecdf's Graph")
+                ecdfs_comparision_graph()
          
     except getopt.error as err:
         print (str(err))
